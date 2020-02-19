13231bd9467b5aa269a64b6467136cce56e59b25
sunjh <1455364690@qq.com>
2020-02-19 16:26:58 +0800

    修改缓存

--- target/classes/static/js/lanmu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	var id=$("#shubusgu").val();
-	var id1=$("#gangwei").val();
-	var id2=$("#player").val();
-	if(id){document.getElementById("shubusgu"+id).className="biabianbian";}
-	if(id1){document.getElementById("gw"+id1).className="biabianbian";}
-	if(id2){document.getElementById("player"+id2).className="biabianbian";}
-});
